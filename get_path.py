e74283658ac4d83e08bcef3167c7aefaa219a5f3
Kenneth Coury <kcoury@redzone.com>
2017-10-11 18:36:04 -0400

    update

--- get_path.py
+++ get_path.py
@@ -2,7 +2,7 @@
 import glob
 
 # 
-img_path = glob.glob(r"C:\Users\gescobar\Documents\SJ501723 - MA Worcester PACP 9-26-2017\LINE\*\*.wmv")
+img_path = glob.glob(r"C:\Users\kcoury\Pictures\*.png")
 
 #print img_path
 
